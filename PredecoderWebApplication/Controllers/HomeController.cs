df094bfce867453438b6e9a8b775b59fa8f7fb8e
Wisdou <dlisay430@gmail.com>
2021-02-23 20:36:26 +0300

    Add project files.

---
+++ PredecoderWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PredecoderWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PredecoderWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OpenMIPSCodePage()
+        {
+            return View(@"/Home/CodePages/MIPSCodePage");
+        }
+        public IActionResult OpenMASMCodePage()
+        {
+            return View(@"/Home/CodePages/MASMCodePage");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
